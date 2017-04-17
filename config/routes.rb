70173ee7deb368565fd80be9dfb0771f1069374a
hoge fuga <hoge@fuga>
2017-04-17 07:08:15 +0000

    ranking

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root to: 'toppages#index'
 
+  get 'rankings/want', to: 'rankings#want'
+  get 'rankings/have', to: 'rankings#have'
+
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
