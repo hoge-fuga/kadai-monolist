e25ff07b3b18d13dd0207579207e1aee533ef305
hoge fuga <hoge@fuga>
2017-04-17 04:52:57 +0000

    want

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,10 @@ class User < ApplicationRecord
                     format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
                     uniqueness: { case_sensitive: false }
   has_secure_password
+  
+  has_many :ownerships
+  has_many :items, through: :ownerships
+  
+  has_many :wants
+  has_many :want_items, through: :wants, class_name: 'Item', source: :item
 end
